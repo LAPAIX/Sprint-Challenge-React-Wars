b1e28accd14fdd0819c4061a9aff4a12ead6ef08
Just Me <lapaix360@hotmail.com>
2019-09-30 18:10:46 -0400

    spring-challenge

---
+++ starwars/Answers.md
@@ -0,0 +1,15 @@
+# Answers
+
+1. What is React JS and what problems does it try and solve? Support your answer with concepts introduced in class and from your personal research on the web.
+
+If the page has data that changes over time at high rates (for example, lots of people commenting on a post, likes being generated. then there is a requirement for DOM updates to be very fast and also reflect in other parts of the UI if they use the same data.
+
+2. What does it mean to think in react?
+   Thinking in react means to think in putting together many different parts to make one big thing.
+3. Describe state.
+   is a hook that allows me to have variables in functional components.
+4. Describe props.
+   Props are objects containing values that are passed to React Components.
+5. What are side effects, and how do you sync effects in a React component to state or prop changes?
+
+side effects modify an effect, we sync this effect through dependencies
--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+//importing the and will be used to show the character
+import CardComponent from "./components/CardComponent";
+import axios from "axios";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  //   // Try to think through what state you'll need for this app before starting. Then build out
+  //   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  //   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
+  //   // side effect in a component, you want to think about which state and/or props it should
+  //   // sync up with, if any.
+
+  //adding the state
+  const [data, setStartWarData] = useState([]);
+
+  //fetching Data
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/") 
+      .then(result => {
+        console.log(result.data);
+        setStartWarData(result.data.results);
+      })
+      .catch(err => {
+        console.log("error: ", err);
+      });
+  }, []); //empty Dependency
 
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
+    <div>
+      {data.map(data => {
+        return (
+          <CardComponent
+            name={data.name}
+            height={data.height}
+            mass={data.mass}
+            hair={data.hair_color}
+          />
+        );
+      })}
     </div>
   );
-}
+
+  //End of the App function
+};
 
 export default App;
